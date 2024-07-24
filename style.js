c6177082b01ac1e6290a1475161b9c35982c4545
Victor Zawacki Monteiro <VictorM@evora.com>
2024-07-24 15:11:59 -0300

    Projeto Petz

---
+++ style.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var flkty = new Flickity('.carousel', {
+        wrapAround: true,
+        // Outras opções de configuração podem ser adicionadas aqui
+    });
+});
