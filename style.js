57d5502836dc9bd31df8527a6d5437338f42c3b9
Victor Zawacki Monteiro <victorzawacki9@gmail.com>
2024-08-16 11:14:54 -0300

    Commit projeto Petz

--- style.js
+++ style.js
@@ -4,3 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
         // Outras opções de configuração podem ser adicionadas aqui
     });
 });
+
+const slider = document.querySelector('.js-slider');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+prevButton.addEventListener('click', () => {
+    slider.scrollBy({ left: -slider.offsetWidth / 4 * 4, behavior: 'smooth' });
+});
+
+nextButton.addEventListener('click', () => {
+    slider.scrollBy({ left: slider.offsetWidth / 4 * 4, behavior: 'smooth' });
+});
