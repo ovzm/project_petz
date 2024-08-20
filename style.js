e25d7e256fddfbe71b9f54f39e3adeb7141dff9b
Victor Zawacki Monteiro <victorzawacki9@gmail.com>
2024-08-20 14:57:35 -0300

    Commit SECTION MORE TEXT

--- style.js
+++ style.js
@@ -16,3 +16,27 @@ prevButton.addEventListener('click', () => {
 nextButton.addEventListener('click', () => {
     slider.scrollBy({ left: slider.offsetWidth / 4 * 4, behavior: 'smooth' });
 });
+
+
+/* ! MORE - DROP BOX */
+function toggleText() {
+    var extraContent = document.getElementById('extra-content');
+    var toggleLink = document.getElementById('toggle-link');
+    var closeLink = document.getElementById('close-link');
+
+    if (extraContent.classList.contains('expanded')) {
+        extraContent.classList.remove('expanded');
+        setTimeout(function() {
+            extraContent.classList.add('hidden');
+        }, 500); // Tempo para coincidir com a duração da animação
+        toggleLink.style.display = "inline";
+        closeLink.style.display = "none";
+    } else {
+        extraContent.classList.remove('hidden');
+        setTimeout(function() {
+            extraContent.classList.add('expanded');
+        }, 10); // Pequeno atraso para garantir que a animação de expansão funcione
+        toggleLink.style.display = "none";
+        closeLink.style.display = "inline";
+    }
+}
